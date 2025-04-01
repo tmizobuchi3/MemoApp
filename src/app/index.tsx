ce7a9ac20c1d99cfec532d66baab32887be1e771
tm92 <83824309+tmizobuchi3@users.noreply.github.com>
2025-04-01 20:13:03 +0900

    60 buttom stayling

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,50 +1,54 @@
 import { View, Text, StyleSheet } from 'react-native'
 
+
+
 const Index = (): JSX.Element => {
     return (
         <View style={styles.container}>
-            <View>
-                <View>
-                    <Text>Memo App</Text>
-                    <Text>ログアウト</Text>
+
+            <View style={styles.header}>
+                <View style={styles.headerInner}>
+                    <Text style={styles.headerTitle}>Memo App</Text>
+                    <Text style={styles.headerRight}>ログアウト</Text>
                 </View>
             </View>
             <View>
 
-                <View>
+                <View style={styles.memoListItem}>
                     <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2020年12月24日 10:00</Text>
+                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                        <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
                     </View>
                     <View>
                         <Text>x</Text>
                     </View>
                 </View>
 
-                <View>
+                <View style={styles.memoListItem}>
                     <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2020年12月24日 10:00</Text>
+                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                        <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
                     </View>
                     <View>
                         <Text>x</Text>
                     </View>
                 </View>
 
-                <View>
+                <View style={styles.memoListItem}>
                     <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2020年12月24日 10:00</Text>
+                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                        <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
                     </View>
                     <View>
                         <Text>x</Text>
                     </View>
                 </View>
+
 
             </View>
 
-            <View>
-                <Text>+</Text>
+            <View style={styles.circleButtom}>
+                <Text style={styles.circleButtomLable}>+</Text>
             </View>
 
         </View>
@@ -54,8 +58,71 @@ const Index = (): JSX.Element => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center', 
-        alignItems: 'center'
+        backgroundColor: "#ffffff"
+    },
+    header: {
+        backgroundColor: "#467FD3",
+        height: 104,
+        justifyContent: "flex-end"
+
+    },
+    headerInner: {
+        alignItems: "center"
+    },
+    headerRight: {
+        position: "absolute",
+        right: 16,
+        bottom: 16,
+        color: "rgba(255,255,255,0.7)"
+    },
+    headerTitle: {
+        marginBottom: 22,
+        fontSize: 22,
+        lineHeight: 32,
+        fontWeight: "bold",
+        color: "#ffffff"
+    },
+    memoListItem: {
+        backgroundColor: "#ffffff",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingVertical: 16,
+        paddingHorizontal: 19,
+        alignItems: "center",
+        borderBottomWidth: 1,
+        borderColor: "rgba(0,0,0,0.15)"
+    },
+    memoListItemTitle: {
+        fontSize: 16,
+        lineHeight: 32
+    },
+    memoListItemDate: {
+        fontSize: 12,
+        lineHeight: 16,
+        color: "#848484"
+        
+    },
+    circleButtom: {
+        width: 64,
+        height: 64,
+        borderRadius: 32,
+        backgroundColor: "#467FD3",
+        justifyContent: "center",
+        alignItems: "center",
+        position: "absolute",
+        right: 40,
+        bottom: 40,
+        
+        shadowColor: "#000000",
+        shadowOpacity: 0.25,
+        shadowRadius: 8,
+        shadowOffset: { width: 0, height: 8 }
+    },
+        circleButtomLable: {
+        color: "#ffffff",
+        fontSize: 40,
+        lineHeight: 40
+
     }
 })
 
